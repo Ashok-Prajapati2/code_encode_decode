d76967e09dd17041ead376a6ab4b8fd06047368e
ashok <ap869631@gmail.com>
2023-08-15 21:22:52 +0530

    Ready @@ code  @@

--- en-de-code.py
+++ en-de-code.py
@@ -4,9 +4,10 @@ alphabet_list = [chr(ord('a') + i) for i in range(26)]*10000
 
 print("Welcone in code encode decode  ")
 
-def encode(user_input,shift):
-    encode = ""
-    
+def both_code(user_input,shift):
+    encode = "" 
+    if user_choose == "d":
+        shift = -(shift)
     for char_is in user_input:
         if char_is in alphabet_list:
           possion = alphabet_list.index(char_is)
@@ -16,7 +17,28 @@ def encode(user_input,shift):
         #   print(f"new possion {new_possion}")
           if new_possion > 25:
                 new_possion -= 26
-                # print(f"new possion >25 {new_possion}")
+          if new_possion < 0:
+                new_possion += 26
+          
+          encode += alphabet_list[new_possion]
+        else :
+            encode += char_is
+    print(encode)
+    
+    
+    
+    
+
+
+def encode(user_input,shift):
+    encode = ""
+    
+    for char_is in user_input:
+        if char_is in alphabet_list:
+          possion = alphabet_list.index(char_is)
+          new_possion = possion+shift
+          if new_possion > 25:
+                new_possion -= 26
           encode += alphabet_list[new_possion]
         else :
             encode += char_is
@@ -28,9 +50,6 @@ def decode(user_input,shift):
         if char_is in alphabet_list:
             possion = alphabet_list.index(char_is)
             new_possion = possion-shift
-            # print(char_is)
-            # print(f"possion {possion}")
-            # print(f"new possion {new_possion}")
             if new_possion < 0:
                 new_possion += 26
                 # print(f"new poss <0 {new_possion}")
@@ -44,11 +63,12 @@ while is_code:
     user_choose = input("For encode : e\nFor decode : d\n").lower()
     user_code = input("Enter code :").lower()
     shift = int(input("Enter shift number : "))
-    
-    if user_choose == "e":
-        encode(user_code, shift)
-    elif user_choose == "d":
-        decode(user_code , shift)
+   
+    both_code(user_code,shift)
+    # if user_choose == "e":
+    #     encode(user_code, shift)
+    # elif user_choose == "d":
+    #     decode(user_code , shift)
 
     retry = input("More code encode , decode ? \nType'y' or 'n' : ").lower()
     if retry == "n":
